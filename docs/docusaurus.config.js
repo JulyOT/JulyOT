e9d92da410b9f470f928107a96a947910c82b709
Jim Bennett <jim.bennett@microsoft.com>
2022-06-24 00:01:43 +0000

    Adding digital swag

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -83,6 +83,7 @@ const config = {
           { to: '/skills', label: 'Cloud Skills', position: 'right'},
           { to: '/roadmap', label: 'Roadmap', position: 'right'},
           { to: '/resources', label: 'Resources', position: 'right'},
+          { to: '/digitalswag', label: 'Digital Swag', position: 'right'},
           {
             type: 'dropdown',
             label: 'Archives',
