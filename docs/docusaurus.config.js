7a013d7665c3a2c94baa6db7fd0341cee3ccf798
Jim Bennett <jim.bennett@microsoft.com>
2022-06-28 21:40:33 +0000

    Removing roadmap

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -84,7 +84,7 @@ const config = {
           { to: '/livestreams', label: 'Live Streams', position: 'right'},
           { to: '/meetups', label: 'Meetups and Events', position: 'right'},
           { to: '/skills', label: 'Cloud Skills', position: 'right'},
-          { to: '/roadmap', label: 'Roadmap', position: 'right'},
+          // { to: '/roadmap', label: 'Roadmap', position: 'right'},
           { to: '/resources', label: 'Resources', position: 'right'},
           { to: '/digitalswag', label: 'Digital Swag', position: 'right'},
           {
